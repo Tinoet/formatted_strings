3afd3d9850325cf58648ed47af0a57eb8e094070
Tinoet <88379020+Tinoet@users.noreply.github.com>
2022-03-20 22:33:01 +0100

    Add files via upload

---
+++ formatted_strings/f_strings.py
@@ -0,0 +1,5 @@
+first_name = 'elijah'
+last_name = 'Tochi'
+msg= f'{first_name} [{last_name}] is a coder'
+print(msg)
+# formatted strings are used to put various values together without making the values look too many to visualize
